b6bc3287a0b8acc36b3b4f2bb4048e359da73a12
Ali Elsisi <you@example.com>
2022-07-18 00:39:53 +0200

    addresses table,its models and controller are added

---
+++ database/migrations/2022_07_17_204142_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string("street");
+            $table->integer("building");
+            $table->integer("floor");
+            $table->integer("flat");
+            $table->string("notes");
+            $table->string("city");
+            $table->string("area");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
