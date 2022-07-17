6d9d1b64bd871b31ce1c76acdadd3c3bf397c525
محمود عبدالباسط <mahmoud01555962838@gmail.com>
2022-07-18 00:20:14 +0200

    suggestions table,model,controller

---
+++ app/Models/Suggestion.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Suggestion extends Model
+{
+    protected $fillable = [
+        'body','photo'
+    ];
+    use HasFactory;
+}
