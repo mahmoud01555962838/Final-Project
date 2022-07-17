6fde7fb28ed6f9dddda2e826e8729a4d1b4f6e22
محمود عبدالباسط <mahmoud01555962838@gmail.com>
2022-07-18 00:13:12 +0200

    category table,model,controller

---
+++ app/Models/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    protected $fillable = [
+        'name','photo'
+    ];
+    use HasFactory;
+}
