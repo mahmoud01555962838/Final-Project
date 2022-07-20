440af1acea7c474a0578723fdd09f750a34c642a
Ramez Romnay <ramezromany1232@gmail.com>
2022-07-20 15:31:30 +0200

    create crud operation of Product and create file aboutus

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -14,9 +15,9 @@ class ProductController extends Controller
      */
     public function index()
     {
-            $products=Product::all();    
-            return view("products/index",["products" => $products]);
-        
+            $products=Product::all();
+            return view("products.index",["products" => $products]);
+
     }
 
     /**
@@ -26,7 +27,8 @@ class ProductController extends Controller
      */
     public function create()
     {
-        //
+        $category = Category::all();
+        return view("users.product.createProduct",["data"=> $category]);
     }
 
     /**
@@ -37,7 +39,39 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // $product=new Product;
+        // $request->validate([
+        //     'name'=>'required' ,
+        //     'description'=>'required',
+        //     'price'=>'required',
+        //     'photo'=>'required',
+        // ]);
+        // $product->user_id=auth()->user()->id;
+        // Product::create($request->all());
+        // return redirect()->route("products.index");
+
+        //wa7de gdeda
+
+        $request->validate([
+            'name' => 'required | min:4',
+            'description' => 'required | min:30',
+            'price' => 'required|numeric',
+            'photo' => 'required|image|mimes:jpg,png,jpeg|max:2048',
+
+
+        ]);
+
+        $path = $request->file('photo')->store('public/images');
+        $product = new Product;
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        $product->photo = $path;
+        $product->category_id = $request->category_id;
+        $product->save();
+     
+        return redirect()->route('products.index')
+                        ->with('success','تمت اضافه المنتج');
     }
 
     /**
@@ -48,7 +82,8 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        
+        return view("users.product.showProduct",["data"=>$product]);
     }
 
     /**
@@ -59,7 +94,9 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        // dd($product);
+        $category = Category::all();
+        return view("users.product.editProduct",["data"=>$product,"data1"=> $category]);
     }
 
     /**
@@ -69,9 +106,37 @@ class ProductController extends Controller
      * @param  \App\Models\Product  $product
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request,$id)
     {
-        //
+        // dd($product);
+    //     $product->update($request->all());
+    //   return redirect()->route("products.index");
+      //w7da gdeda
+      $request->validate([
+            'name' => 'required | min:4',
+            'description' => 'required | min:30',
+            'price' => 'required|numeric ',
+            // 'photo' => 'required|image|mimes:jpg,png,jpeg|max:2048'
+        
+    ]);
+  
+    $product = Product::find($id);
+    if($request->hasFile('photo')){
+        $request->validate([
+          'photo' => 'required|image|mimes:jpg,png,jpeg|max:2048',
+        ]);
+        $path = $request->file('photo')->store('public/images');
+        $product->photo = $path;
+    }
+        $product->name = $request->name;
+        $product->description = $request->description;
+        $product->price = $request->price;
+        // $product->photo = $path;
+        $product->photo = $request->photo;
+        $product->category_id = $request->category_id;
+    $product->save();
+    return redirect()->route('products.index')
+                    ->with('success','تم تعديل المنتج');
     }
 
     /**
@@ -82,6 +147,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+       return redirect()->route("products.index");
     }
 }
