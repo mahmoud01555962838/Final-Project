3512cf9642dca61f81a06f1c10ba597fcb4138e5
RamezRomany <ramezromany1232@gmail.com>
2022-07-22 12:14:54 +0200

    upadte from nav

--- resources/views/users/product/editproduct.blade.php
+++ resources/views/users/product/editproduct.blade.php
@@ -45,7 +45,5 @@
     <button type="submit" class="btn btn-primary">Update</button>
     </div>
   </form>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-</body>
-</html>
+  
 @endsection
