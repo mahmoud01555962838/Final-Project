cae4fca5e06dbd3a4eafb37cfe557df0d3d41285
RamezRomany <ramezromany1232@gmail.com>
2022-07-27 21:32:09 +0200

    upadte for desgin

--- resources/views/filter.blade.php
+++ resources/views/filter.blade.php
@@ -1,8 +1,23 @@
 @extends('master')
 @section("content")
-<div class="carousel container" data-flickity>
+<section class="container" >
+    <div class="row row-cols-1 row-cols-md-3 g-4 mt-3 container">
           @foreach ($products as $item)
-              <a href="{{route("products.show",[$item->id])}}"><img class="carousel-cell" src="{{Storage::url($item->photo)}}"/></a>
+          <div class="col-md-4 ">
+
+<div class="card">
+  
+     <a href="{{route("products.show",[$item->id])}}"><img src="{{Storage::url($item->photo)}}" class="card-img-top "  alt="..." style="height: 20rem;"></a>
+
+     <div class="card-body">
+          <h2 class="card-title" style="color:#232865">{{$item->name}}</h2>
+          <p class="card-text">{{$item->description}}</p>
+          <p class="card-text">{{$item->price}} EGP </p>
+      </div>
+</div>
+</div>
           @endforeach
+          </div>
+  </section>
 </div>
 @endsection
